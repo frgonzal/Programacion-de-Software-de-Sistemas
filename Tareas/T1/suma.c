c421132ec4b255160cf1a562a3b6a1a002dc530d
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-08-20 11:49:48 -0400

    cambios en los nombres de variables

--- Tareas/T1/suma.c
+++ Tareas/T1/suma.c
@@ -4,30 +4,30 @@
 #include "suma.h"
 
 Bcd sumaBcd(Bcd x, Bcd y) {
-    unsigned int mascara = 15; // == 0b1111
-    unsigned int resto = 0;
+    unsigned int mascara = 15; // == 0b1111, para obtener los ultimos 4 bits
+    unsigned int carry = 0;    // para guardar lo que sobra en cada suma
 
-    Bcd m;
-    Bcd z = 0;
+    Bcd suma;
+    Bcd ans = 0;
 
-    for(int i=0; i<16; i++){
+    for(int i=0; i<64; i+=4){//moverse 4 bits en cada iteracion
 
         //mover los bits a sumar a las primeras 4 posiciones y aplicar mascara
-        m = ((x>>i*4) & mascara) + ((y>>i*4) & mascara) + resto;
-        resto = 0;
+        suma  = ((x>>i) & mascara) + ((y>>i) & mascara) + carry;
+        carry = 0;
 
         //si el numero es mayor a 9, se le resta 10 y guardamos lo que sobra
-        if (m > 9){
-            m -= 10;
-            resto = 1;
+        if(suma > 9){
+            suma -= 10;
+            carry = 1;
         }
 
         //sumar en la posicion correspondiente
-        z += m<<i*4;
+        ans += suma<<i;
     }
 
-    if(!resto)//si el resto no es 0 ==> desborde
-        return z;
+    if(!carry)//si el carry no es 0 ==> desborde
+        return ans;
     else
         return 0xffffffffffffffff;
 }
