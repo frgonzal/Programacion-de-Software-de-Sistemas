7688fdf100262b51c19223bd2557dcb082ae85d1
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-09-05 15:56:02 -0300

    (tareas)

--- Tareas/T2/elim.c
+++ Tareas/T2/elim.c
@@ -5,75 +5,56 @@
 
 #include "elim.h"
 
-unsigned int contar_espacio(char *str, unsigned int ns, char *pat, unsigned int np);
+unsigned int contar_espacio(char *str, char *pat, unsigned int np);
 
 void eliminar(char *str, char *pat) {
-    unsigned int ns = strlen(str);
     unsigned int np = strlen(pat);
-    if(ns < np || !np)//casos en los que no tiene sentido seguir
+    if(!np)//caso en el que no tiene sentido seguir
         return;
-
     char *ini = str;
-    char *end = str+np;
-    char *end_str = str+ns;
 
-    while(end <= end_str){
+    while(*ini){
         if(*ini==*pat && !strncmp(ini+1, pat+1, np-1)){
             ini+=np;
-            end+=np;
         }else{
             *str++ = *ini++;
-            end++;
         }
     }
-    while(ini<=end_str)
-        *str++=*ini++;
+    *str = 0;
 }
 
 char *eliminados(char *str, char *pat) {
-    unsigned int ns = strlen(str);
     unsigned int np = strlen(pat);
     char *str2;
-    if(ns<np || !np ){
-        str2 = (char*)malloc(ns+1);
+    if(!np ){
+        str2 = (char*)malloc(strlen(str)+1);
         strcpy(str2, str);
         return str2;
     }
-    str2 = (char*)malloc(contar_espacio(str, ns, pat, np)*sizeof(char));
+    str2 = (char*)malloc(contar_espacio(str, pat, np)*sizeof(char));
     char *aux = str2;
-
     char *ini = str;
-    char *end = str+np;
-    char *end_str = str+ns;
 
-    while(end<=end_str){
+    while(*ini){
         if(*ini==*pat && !strncmp(ini+1, pat+1, np-1)){
             ini += np;
-            end += np;
         }else{
             *aux++ = *ini++;
-            end++;
         }
     }
-    while(ini<end_str)
-        *aux++=*ini++;
     *aux=0;
     return str2;
 }
 
-unsigned int contar_espacio(char *str, unsigned int ns, char *pat, unsigned int np){
-    unsigned int contador = 0;
-    char *end     = str+np;
-    char *end_str = str+ns;
-    while(end<=end_str){
+unsigned int contar_espacio(char *str, char *pat, unsigned int np){
+    unsigned int contador = 1;
+    while(*str){
         if(*str==*pat && !strncmp(str+1, pat+1, np-1)){
             str+=np;
-            end+=np;
         }else{
             contador++;
             str++;
-            end++;
         }
     }
-    return contador + np;
+    return contador;
 }
