8ccbe5193a1a4896936e94e84b7467d7245b5206
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-10-05 16:33:17 -0300

    (tarea4)

--- Tareas/T4/definir.c
+++ Tareas/T4/definir.c
@@ -6,47 +6,47 @@
 
 #include "pss.h"
 
-
 int main(int argc, char *argv[]) {
+    int lineSize = 81;
+    char buf[lineSize+1];
+    char *buf_r;
+
+    /* comprobar argumentos del main */
     if(argc != 4){
         fprintf(stderr, "Uso: ./definir <diccionario> <llave> <definicion>\n");
         return 1;
     }
-
     char *fileName   = argv[1];
     char *key        = argv[2];
     char *definition = argv[3];
 
+    /* abrir archivo */
     FILE *fp = fopen(fileName, "r+");
-    if(fp == NULL){
+    if(fp == NULL){// no se abrio nada
         perror(fileName);
         return 1;
     } 
 
-    int lineSize = 81;
-    char buf[lineSize+1];
-    buf[lineSize] = 0;
-    char *buf_r;
+    /* ultimo caracter == 0 */
+    *(buf+lineSize) = 0;
 
     /* numero de lineas */
     fseek(fp, 0, SEEK_END);
     int fileSize = ftell(fp)/lineSize;
 
+    /* linea a leer */
     int line = hash_string(key)%fileSize;
     int initial_line = line;
-
+    /* moverse a la linea */
     fseek(fp, line*lineSize, SEEK_SET);
 
     for(;;){
-
         buf_r = buf;
         if(fread(buf, sizeof(char), lineSize, fp) > 0){
 
             if(*buf_r == ' '){//si el primer valor es un espacio
-                /*
-                 volver puntero al inicio de la linea y copiar
-                 <llave>:<def>...\n
-                */
+                /* volver puntero al inicio de la linea y copiar
+                   <llave>:<def>...\n */
                 fseek(fp, -lineSize, SEEK_CUR);
                 fwrite(key, sizeof(char), strlen(key), fp);
                 fputc(':', fp);
@@ -54,10 +54,12 @@ int main(int argc, char *argv[]) {
                 break;
 
             }else{//no es vacio
-                while(*buf_r != ':' && *buf_r != 0)
+                /* dejar solo la llave */
+                while(*buf_r!=':' && *buf_r)
                     buf_r++;
                 *buf_r = 0;
 
+                /* comparar llave */
                 if( !strcmp(buf, key) ){//ya esta
                     fprintf(stderr, "La llave %s ya se encuentra en el diccionario\n", key);
                     fclose(fp);
