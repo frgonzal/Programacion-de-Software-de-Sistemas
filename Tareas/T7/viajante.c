dc6d4846a816c07fe0faccf16e11570ff89cf985
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-11-17 21:54:13 -0300

    tarea(7): terminada

--- Tareas/T7/viajante.c
+++ Tareas/T7/viajante.c
@@ -11,8 +11,10 @@
 #include "viajante.h"
 
 int leer(int fd, void *vbuf, int n);
+static double dist(int z[], int n, double **m);
 
 double viajante_par(int z[], int n, double **m, int nperm, int p) {
+    double min = DBL_MAX;
     // Complete esta funcion
     // Puede invocar a su version secuencial: viajante
     // Use la funcion leer para que el padre reciba la ruta del hijo.
@@ -22,26 +24,56 @@ double viajante_par(int z[], int n, double **m, int nperm, int p) {
     // Despues de invocar fork() agregue: srandom(getUSecsOfDay()*getpid());
     // Esto es para que cada proceso genere secuencias de numeros aleatorios
     // diferentes.
-    double min= DBL_MAX; // la menor distancia hasta el momento
 
-    for (int i= 1; i<=nperm; i++) {
+    pid_t pids[p];
+    int fds[p][2]; 
 
-        int x[n+1];          // almacenará una ruta aleatoria
-        gen_ruta_alea(x, n); // genera ruta x[0]=0, x[1], x[2], ..., x[n], x[0]=0
+    for(int i=0; i<p; i++){
+        pipe(fds[i]);
+        pids[i] = fork();
+        srandom(getUSecsOfDay()*getpid());
 
-        // calcula la distancia al recorrer 0, x[1], ..., x[n], 0
-        double d= dist(x, n, m);
 
-        if (d<min) {      // si distancia es menor que la que se tenía hasta el momento
-            min= d;       // d es la nueva menor distancia
-            for (int j= 0; j<=n; j++)
-            z[j]= x[j];   // guarda ruta que recorre la menor distancia en parámetro z
+        //calcular valor para los hijos
+        if (pids[i] == 0){//hijo
+            close(fds[i][0]);
+
+
+            int x[n+1];
+            viajante(x, n, m, nperm/p);
+
+            write(fds[i][1], &x, (n+1)*sizeof(int));
+            exit(0);
+        }else{
+            close(fds[i][1]);//padre no escribe
         }
     }
+
+    for(int i=0; i<p; i++){
+        int x[n+1];
+
+        leer(fds[i][0], &x, (n+1)*sizeof(int));
+        close(fds[i][0]);
+        waitpid(pids[i], NULL, 0);
+
+        double d = dist(x, n, m);
+        if(d < min){
+            min = d;
+            for(int j=0; j<=n; j++)
+                z[j] = x[j];
+        }
+    }
+
     return min;
 }
 
-
+static double dist(int z[], int n, double **m) {
+  double d= m[z[n]][0];    // distancia de z[n] a 0
+  for (int i=0; i<n; i++) {
+      d += m[z[i]][z[i+1]];
+  }
+  return d;
+}
 
 
 
